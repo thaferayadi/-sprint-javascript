0932e531af134d9aebb6b68e80747ef0b369b645
Ayadi Mouhamed Thafer <thaferayadi@gmail.com>
2023-12-24 15:06:30 +0100

    first javascript

---
+++ sprint1.js
@@ -0,0 +1,61 @@
+//exercice1
+var carName="Volvo";
+
+//exercice2
+var length = 16;
+
+var lastName = "Johnson"; 
+
+var isGreaterThan10 = length > 10 
+
+//exercice3
+ //corrige1
+function square1(x) { 
+    return x * x ; 
+    } 
+    //corrige2
+    function square2 (x) {
+return x * x ; 
+}
+ //corrige3
+ function square3(x) { 
+    return x * x;
+    }
+    //exercice4
+    function largest(a,b) {
+        if (a>b){
+            return 'a is the largest variable'
+        } 
+        else{
+            return 'b is the largest variable'
+        }
+      }
+    //exercice5
+    function subtract(number1, number2) {
+return number2-number1;
+    }
+    function add(number1, number2) {
+        return number1-number2;
+            }
+      var x = '+';
+    var number1="5"
+    var number2="6"
+var resultat;
+      if (x === '+') {
+   resultat=add(number1, number2);
+} else {
+
+    resultat=subtract(number1, number2);
+}
+    //exercice6
+
+function divide(a,b) {
+    return a/b;
+}
+
+
+function multiply() {
+return a*b;
+}
+
+
